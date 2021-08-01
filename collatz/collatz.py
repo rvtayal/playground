db550a3d5ca4f336439feb1c5574e0f78bbb81ae
RJ Tayal <rvtayal@gatech.edu>
2021-08-01 12:34:28 -0400

    functions for generating collatz plots

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -1,3 +1,12 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+
+# variables
+ANGLE_DIVIDE = -math.pi / 18
+ANGLE_MUL = math.pi / 9
+
 def get_collatz_path(num):
     path = [num]
     cur = num
@@ -10,7 +19,52 @@ def get_collatz_path(num):
     return path
 
 
+def collatz_path_to_graph_points(path):
+    path = path[::-1]
+    g_points = np.zeros((2,len(path)))
+    prev = 1
+    angle = 0
+    dist = 1
+    for i in range(1, len(path)):
+        cur = path[i]
+
+        if cur > prev:
+            # divided by 2
+            angle += ANGLE_DIVIDE
+        else:
+            # 3x+1
+            angle += ANGLE_MUL
+        delta = np.array([dist * math.cos(angle), dist * math.sin(angle)]).reshape(2,)
+
+        g_points[:, i] = g_points[:, i-1] + delta
+        prev = cur
+        dist *= 0.99
+
+    ret_points = np.zeros((2,2, len(path)-1))
+    for i in range(len(path)-1):
+        ret_points[:,:, i] = g_points[:, [i, i+1]]
+    return ret_points
+
+
+def aggregate_points(nums):
+    path = get_collatz_path(nums[0])
+    points = collatz_path_to_graph_points(path)
+    for num in nums[1:]:
+        path = get_collatz_path(num)
+        points = np.concatenate((points, collatz_path_to_graph_points(path)), axis=2)
+    return points
+
+
+def plot_points(points):
+    fig, ax = plt.subplots()
+    for i in range(points.shape[-1]):
+        ax.plot(points[0,:, i], points[1,:, i])
+    ax.axis('equal')
+    plt.show()
+
+
 if __name__ == "__main__":
-    print(get_collatz_path(4))
-    print(get_collatz_path(1))
-    print(get_collatz_path(15))
+    # nums = [2,4,17]
+    nums = list(range(2,500))
+    points = aggregate_points(nums)
+    plot_points(points)
